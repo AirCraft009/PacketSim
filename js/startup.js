0011852eda65f9ff5ddecf0ad8bb27d502481663
AirCraft009 <coconraupe@gmail.com>
2025-12-26 20:48:50 +0100

    updated editor Layout max devices check for Networks

--- js/startup.js
+++ js/startup.js
@@ -2,7 +2,7 @@ import * as Utils from "./util.js"
 import * as Network from "./network.js"
 
 const grid = document.getElementById("grid");
-const editBox = document.getElementById("edit-box");
+const editBox = document.getElementById("editor");
 
 var connectingMode = false;
 var connStart;
@@ -13,15 +13,35 @@ var draggedTemplate = null;
 let komponenten;
 let network = new Network.Network(24, new Network.ip("192.168.9.3"));
 
+// default state for the editor on the right
+// also used for rendering new information
+const state = {
+  ip: "192.168.3.4",
+  type: "static",
+  mac: "aa:bb:cc:dd:ee:ff",
+  connection: "ethernet",
+  gateway: "192.168.3.1"
+};
 
 
 
+/**
+ * Initialize document listeners for drag and drop and input disabling
+ */
+export function InitDocumentListeners() {
+  initDocumentDrag();
+  disableInput();
+}
 
 
-// create cells & core visual functionality
-// drag and drop, click to connect/remove
-// right click to remove component
-// n = number of cells
+
+/**
+ * create cells & core visual functionality\
+ * drag and drop, click to connect/components\
+ * right click to remove component
+ * 
+ * @param {ammount of cells} n 
+ */
 export function createGrid(n) {
   
   komponenten = Array.apply(null, Array(n)).map(function () { });
@@ -30,7 +50,6 @@ export function createGrid(n) {
     const cell = document.createElement("div");
     cell.className = "cell";
 
-
     // set basic drag and drop listeners
     setDragListeners(cell, i);
     handleMouseClick(cell, i);
@@ -38,44 +57,6 @@ export function createGrid(n) {
     grid.appendChild(cell);
   }
 }
-
-
-
-
-
-
-
-
-function handleMouseClick(cell, i){
-  cell.addEventListener("mousedown", (e) => {
-    resetHighlight();
-    if(hasChild(cell)) {
-      if (e.button == 2) {
-        removeComponent(cell, i);
-      }
-      if (e.button == 0) { 
-        connectComponents(i);
-      }
-      highlightCell();
-    }
-    });
-}
-
-function highlightCell(){
-  if(connectingMode){
-    var cell = connStart.cell;
-    cell.style.backgroundColor = "blue";
-    selected = true;
-    openEditBox();
-  }
-}
-
-
-
-
-
-
-
 
 
 // utility functions for component management and visual connection
@@ -100,40 +81,6 @@ function connectComponents(i) {
     }
     connStart = komponenten[i];
     connectingMode = true;
-}
-
-function hasChild(cell){
-  if (!cell.firstChild) {
-    connectingMode = false;
-    return false;
-  }
-  return true;
-}
-function initDocumentDrag() {
-  document.addEventListener("dragstart", (e) => {
-    if (e.target.dataset.template) {
-      draggedTemplate = e.target;
-    }
-  });
-
-  document.addEventListener("dragend", () => {
-    draggedTemplate = null;
-  });
-}
-
-function disableInput() {
-  window.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-  });
-
-  window.addEventListener("keydown", (e) => {
-    connectingMode = false;
-  });
-}
-
-export function InitDocumentListeners() {
-  initDocumentDrag();
-  disableInput();
 }
 
 function setDragListeners(cell, index) {
@@ -173,25 +120,37 @@ function setDragListeners(cell, index) {
   });
 }
 
-function isvalidConnection(index) {
-  if (connectingMode) {
-    connectingMode = false;
-    // check for connection to self
-    if (connStart.index == index) return false;
-    // check if connection already exists
-    if (komponenten[index].connections.has(connStart.index)) {
-      return false;
+function initDocumentDrag() {
+  document.addEventListener("dragstart", (e) => {
+    if (e.target.dataset.template) {
+      draggedTemplate = e.target;
     }
-    return true;
-  }
-  return false;
+  });
+
+  document.addEventListener("dragend", () => {
+    draggedTemplate = null;
+  });
 }
 
-function addConnection(komponent, index){
-    Utils.drawLine(komponent, connStart, index);
+function disableInput() {
+  window.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
 
-    connStart.connections.add(index);
-    komponenten[index].connections.add(index);
+  window.addEventListener("keydown", (e) => {
+    connectingMode = false;
+  });
+
+  // prevent new lines cause they look bad
+  document.querySelectorAll(".editable").forEach((element) => {
+    element.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        // remove focus from the element
+        element.blur();
+      }
+    });
+  });
 }
 
 function resetSimulation() {
@@ -207,12 +166,81 @@ function openEditBox() {
   if (!selected || connStart == null) {
     return;
   } 
+  updateState(connStart);
+  renderEditBox();
+}
 
-  editBox.style.display = "block";
-  editBox.innerHTML = `
-    <h5>Component Editor</h5>
-    <p>Type: ${connStart.type}</p>
-    <p>Connections: ${Array.from(connStart.connections).length}</p>
-  `;
+function updateState(komponent){
+  state.ip = komponent.ipAddress.toString();
+  state.type = komponent.type;
+  state.connection = komponent.connections.size;
+  //TODO: Implement MAC-Adress and Default Gateway
+  // state.mac = komponent.macAddress;
+  // state.gateway = komponent.gateway;
+}
+
+
+// renders new information to the edit box
+function renderEditBox() {
+  document.querySelectorAll(".editable").forEach(el => {
+    const key = el.dataset.key;
+    if (key in state) {
+      el.textContent = state[key];
+    }
+  });
+}
+
+
+function handleMouseClick(cell, i){
+  cell.addEventListener("mousedown", (e) => {
+    resetHighlight();
+    if(hasChild(cell)) {
+      if (e.button == 2) {
+        removeComponent(cell, i);
+      }
+      if (e.button == 0) { 
+        connectComponents(i);
+      }
+      highlightCell();
+    }
+    });
+}
+
+function highlightCell(){
+  if(connectingMode){
+    var cell = connStart.cell;
+    cell.style.backgroundColor = "blue";
+    selected = true;
+    openEditBox();
+  }
+}
+
+function addConnection(komponent, index){
+    Utils.drawLine(komponent, connStart, index);
+
+    connStart.connections.add(index);
+    komponenten[index].connections.add(index);
+}
+
+function isvalidConnection(index) {
+  if (connectingMode) {
+    connectingMode = false;
+    // check for connection to self
+    if (connStart.index == index) return false;
+    // check if connection already exists
+    if (komponenten[index].connections.has(connStart.index)) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+function hasChild(cell){
+  if (!cell.firstChild) {
+    connectingMode = false;
+    return false;
+  }
+  return true;
 }
 
