e22a607073feecfdd7cfe81103e67f06afbbfaf9
mxsxll <coconraupe@gmail.com>
2026-01-08 16:57:00 +0100

    removed home page added help form and imprint

--- js/simulator.js
+++ js/simulator.js
@@ -2,4 +2,4 @@ import * as main from "./main.js";
 import * as Network from "./network.js";
 console.log(new Network.macAddress().toString());
 main.InitDocumentListeners();
-main.createGrid(64);
+main.createGrid(121);
